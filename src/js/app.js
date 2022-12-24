c36f2d1c032bd0c09ef7f28b1de477585c7de95b
Oczy1 <zik2la@gmail.com>
2022-12-24 15:06:53 +0300

    gen

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import * as oczyFunctions from "./modules/functions.js";
+
+
+//oczyFunctions.isWebp();
+
+
+oczyFunctions.scrollclick();
+
+
+oczyFunctions.parallax();
+
+
+oczyFunctions.jquery();
+
+oczyFunctions.parallaxscroll();
+
+//oczyFunctions.txtanimate();
